6c79a863ec34fc9c233d79b722f487d2bc9af96f
zaydon369 <zaydon369@yahoo.com>
2015-10-21 23:05:57 +0800

    设置更新按钮成功设置状态选择器,点击布局的任意位置可改变背景颜色,可改变状态

--- src/com/zheng/mobilesafe/activities/SettingActivity.java
+++ src/com/zheng/mobilesafe/activities/SettingActivity.java
@@ -10,24 +10,31 @@ import android.view.View;
 import android.view.ContextMenu.ContextMenuInfo;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 public class SettingActivity extends Activity {
 	//定义布局控件
 	private SwitchImageView iv_setting_update;
-	
-	
+	RelativeLayout rl_setting_update;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_setting);
 		//初始化控件
 		iv_setting_update=(SwitchImageView) findViewById(R.id.iv_setting_update);
-		//设置图片点击事件,用于更改状态
-		iv_setting_update.setOnClickListener(new OnClickListener(){
+		rl_setting_update=(RelativeLayout) findViewById(R.id.rl_setting_update);
+		//给相对布局设置点击事件
+		rl_setting_update.setOnClickListener(new OnClickListener(){
 			@Override
 			public void onClick(View v) {
 				iv_setting_update.changedSwitchStatus();
 			}});
+//		//设置图片点击事件,用于更改状态
+//		iv_setting_update.setOnClickListener(new OnClickListener(){
+//			@Override
+//			public void onClick(View v) {
+//				iv_setting_update.changedSwitchStatus();
+//			}});
 		
 	}
 
