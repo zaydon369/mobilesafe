ac297c123d0be706403c9490fac82ce8151ccaa7
zaydon369 <zaydon369@yahoo.com>
2015-10-22 12:33:02 +0800

    完成密码对话框的基本逻辑设置

--- src/com/zheng/mobilesafe/activities/HomeActivity.java
+++ src/com/zheng/mobilesafe/activities/HomeActivity.java
@@ -6,17 +6,22 @@ import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.zheng.mobilesafe.R;
 
@@ -26,8 +31,10 @@ public class HomeActivity extends Activity {
 	private ImageView iv_home_logo;
 	// 定义gridview功能界面的控件
 	private GridView gv_home_item;
-	//定义共享参数sp
+	// 定义共享参数sp
 	private SharedPreferences sp;
+	// 对话框
+	private AlertDialog dialog;
 	// 设置功能的名称
 	String names[] = new String[] { "手机防盗", "骚扰拦截", "软件管家", "进程管理", "流量统计",
 			"手机杀毒", "系统加速", "常用工具" };
@@ -38,7 +45,6 @@ public class HomeActivity extends Activity {
 	// 设置功能的描述
 	String descs[] = new String[] { "远程定位手机", "全面拦截骚扰", "管理您的软件", "管理正在运行",
 			"流量一目了然", "病毒无处藏身", "系统快如火箭", "常用工具大全" };
-	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -49,8 +55,8 @@ public class HomeActivity extends Activity {
 		iv_home_logo = (ImageView) findViewById(R.id.iv_home_logo);
 		// 初始化gridview
 		gv_home_item = (GridView) findViewById(R.id.gv_home_item);
-		//初始化共享参数sp
-		sp=getSharedPreferences("config", MODE_PRIVATE);
+		// 初始化共享参数sp
+		sp = getSharedPreferences("config", MODE_PRIVATE);
 		// iv_home_logo.setRotationY(rotationY);
 		// 设置属性动画
 		ObjectAnimator oa = ObjectAnimator.ofFloat(iv_home_logo, "rotationY",
@@ -72,11 +78,11 @@ public class HomeActivity extends Activity {
 					int position, long id) {
 				switch (position) {
 				case 0:// "手机防盗"
-					//判断是否设置过密码
-					if(isSetupPwd()){//设置过密码,弹出对话框,提示输入密码
+						// 判断是否设置过密码
+					if (isSetupPwd()) {// 设置过密码,弹出对话框,提示输入密码
 						Log.i(TAG, "提示输入密码");
 						showEnterPwdDialog();
-					}else{//没设置过密码,弹出对话框,提示设置密码
+					} else {// 没设置过密码,弹出对话框,提示设置密码
 						Log.i(TAG, "提示设置密码");
 						showSetupPwdDialog();
 					}
@@ -108,41 +114,116 @@ public class HomeActivity extends Activity {
 		});
 
 	}
+
 	/**
 	 * 显示输入密码对话框
 	 */
-	protected void showEnterPwdDialog() {
-		AlertDialog.Builder builder=new Builder(this);
-		View view =View.inflate(this, R.layout.dialog_enter_pwd, null);
+	private void showEnterPwdDialog() {
+		AlertDialog.Builder builder = new Builder(this);
+		final View view = View.inflate(this, R.layout.dialog_enter_pwd, null);
 		builder.setView(view);
-		builder.show();
+		dialog = builder.show();
+		// 获取按钮点击事件
+		Button bt_cancel = (Button) view.findViewById(R.id.bt_dialog_cancel);
+		// 点击取消按钮
+		bt_cancel.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				dialog.dismiss();
+			}
+		});
+		Button bt_ok = (Button) view.findViewById(R.id.bt_dialog_ok);
+		// 点击确定按钮
+		bt_ok.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				// 查找用户输入的密码
+				EditText et_dialog_enterPwd = (EditText) view.findViewById(R.id.et_dialog_enterPwd);
+				String enterPwd = et_dialog_enterPwd.getText().toString().trim();
+				if (TextUtils.isEmpty(enterPwd)) {
+					Toast.makeText(HomeActivity.this, "请输入密码", 0).show();
+					return;
+				}
+				// 如果和配置文件的不一致
+				if (!enterPwd.equals(sp.getString("password", null))) {
+					Toast.makeText(HomeActivity.this, "密码输入错误", 0).show();
+					return;
+				}
+				dialog.dismiss();
+				// TODO 进入界面 ,未写
+			}
+		});
+
+	
 	}
+
 	/**
 	 * 显示设置密码对话框
 	 */
 	private void showSetupPwdDialog() {
-		AlertDialog.Builder builder=new Builder(this);
-		View view=View.inflate(this, R.layout.dialog_setup_pwd, null);
+
+		Builder builder = new Builder(this);
+		View view = View.inflate(this, R.layout.dialog_setup_pwd, null);
 		builder.setView(view);
-		builder.show();
-		
-	}
-	/**
-	 * 是否设置过密码
-	 * @return
-	 */
-	protected boolean isSetupPwd() {
-		//读取设置文件
-		String password=sp.getString("password", null);
-		if(TextUtils.isEmpty(password)){//如果密码为空
-			return false;	
-		}else{
-			return true;
-		}
-		
+		dialog = builder.show();
+		final EditText et_pwd = (EditText) view
+				.findViewById(R.id.et_dialog_pwd);
+		final EditText et_pwd_confirm = (EditText) view
+				.findViewById(R.id.et_dialog_pwd_confirm);
+		// 获取按钮点击事件
+		Button bt_ok = (Button) view.findViewById(R.id.bt_dialog_ok);
+		// 点击确定按钮
+		bt_ok.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// 找到密码输入框,匹配两次密码是否一致
+				String pwd = et_pwd.getText().toString().trim();
+				String pwd_confirm = et_pwd_confirm.getText().toString().trim();
+				// 如果有一个为空
+				if (TextUtils.isEmpty(pwd) || TextUtils.isEmpty(pwd_confirm)) {
+					Toast.makeText(HomeActivity.this, "密码不能为空", 0).show();
+					return;
+				}
+				if (!pwd.equals(pwd_confirm)) {
+					Toast.makeText(HomeActivity.this, "两次密码不一致", 0).show();
+					return;
+				}
+				Editor editor = sp.edit();
+				editor.putString("password", pwd);
+				editor.commit();
+				dialog.dismiss();
+			}
+		});
+		Button bt_cancel = (Button) view.findViewById(R.id.bt_dialog_cancel);
+		// 点击取消按钮
+		bt_cancel.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				dialog.dismiss();
+			}
+		});
 	}
 
 	/**
+	 * 是否设置过密码
+	 * 
+	 * @return
+	 */
+	protected boolean isSetupPwd() {
+		// 读取设置文件
+		String password = sp.getString("password", null);
+		if (TextUtils.isEmpty(password)) {// 如果密码为空
+			return false;
+		} else {
+			return true;
+		}
+
+	}
+
+	/**
+	 * gridview的布局适配器
 	 * 
 	 * @author asus
 	 * 
