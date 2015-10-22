f21833d641d04744977b0cd64245ddaf0e17fd6d
zaydon369 <zaydon369@yahoo.com>
2015-10-22 11:35:43 +0800

    完成自定义输入框提示输入密码.

--- src/com/zheng/mobilesafe/activities/HomeActivity.java
+++ src/com/zheng/mobilesafe/activities/HomeActivity.java
@@ -2,10 +2,17 @@ package com.zheng.mobilesafe.activities;
 
 import android.animation.ObjectAnimator;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
@@ -14,20 +21,25 @@ import android.widget.TextView;
 import com.zheng.mobilesafe.R;
 
 public class HomeActivity extends Activity {
+	private static final String TAG = "HomeActivity";
 	// 定义Logo控件
 	private ImageView iv_home_logo;
 	// 定义gridview功能界面的控件
 	private GridView gv_home_item;
+	//定义共享参数sp
+	private SharedPreferences sp;
 	// 设置功能的名称
 	String names[] = new String[] { "手机防盗", "骚扰拦截", "软件管家", "进程管理", "流量统计",
 			"手机杀毒", "系统加速", "常用工具" };
 	// 设置功能的图标
 	int icons[] = new int[] { R.drawable.sjfd, R.drawable.srlj,
-			R.drawable.rjgj, R.drawable.jcgl, R.drawable.lltj,
-			R.drawable.sjsd, R.drawable.xtjs, R.drawable.cygj };
+			R.drawable.rjgj, R.drawable.jcgl, R.drawable.lltj, R.drawable.sjsd,
+			R.drawable.xtjs, R.drawable.cygj };
 	// 设置功能的描述
 	String descs[] = new String[] { "远程定位手机", "全面拦截骚扰", "管理您的软件", "管理正在运行",
 			"流量一目了然", "病毒无处藏身", "系统快如火箭", "常用工具大全" };
+	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -37,7 +49,8 @@ public class HomeActivity extends Activity {
 		iv_home_logo = (ImageView) findViewById(R.id.iv_home_logo);
 		// 初始化gridview
 		gv_home_item = (GridView) findViewById(R.id.gv_home_item);
-	
+		//初始化共享参数sp
+		sp=getSharedPreferences("config", MODE_PRIVATE);
 		// iv_home_logo.setRotationY(rotationY);
 		// 设置属性动画
 		ObjectAnimator oa = ObjectAnimator.ofFloat(iv_home_logo, "rotationY",
@@ -52,13 +65,88 @@ public class HomeActivity extends Activity {
 		oa.start();
 		// 设置gv布局
 		gv_home_item.setAdapter(new HomeAdapter());
+		// 设置gv的条目点击事件
+		gv_home_item.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				switch (position) {
+				case 0:// "手机防盗"
+					//判断是否设置过密码
+					if(isSetupPwd()){//设置过密码,弹出对话框,提示输入密码
+						Log.i(TAG, "提示输入密码");
+						showEnterPwdDialog();
+					}else{//没设置过密码,弹出对话框,提示设置密码
+						Log.i(TAG, "提示设置密码");
+						showSetupPwdDialog();
+					}
+					break;
+
+				case 1:// "骚扰拦截"
+
+					break;
+				case 2:// "软件管家"
+
+					break;
+				case 3:// "进程管理"
+
+					break;
+				case 4:// "流量统计"
+
+					break;
+				case 5:// "手机杀毒"
+
+					break;
+				case 6:// "系统加速"
+
+					break;
+				case 7:// "常用工具"
+
+					break;
+				}
+			}
+		});
 
 	}
-/**
- * 
- * @author asus
- *
- */
+	/**
+	 * 显示输入密码对话框
+	 */
+	protected void showEnterPwdDialog() {
+		AlertDialog.Builder builder=new Builder(this);
+		View view =View.inflate(this, R.layout.dialog_enter_pwd, null);
+		builder.setView(view);
+		builder.show();
+	}
+	/**
+	 * 显示设置密码对话框
+	 */
+	private void showSetupPwdDialog() {
+		AlertDialog.Builder builder=new Builder(this);
+		View view=View.inflate(this, R.layout.dialog_setup_pwd, null);
+		builder.setView(view);
+		builder.show();
+		
+	}
+	/**
+	 * 是否设置过密码
+	 * @return
+	 */
+	protected boolean isSetupPwd() {
+		//读取设置文件
+		String password=sp.getString("password", null);
+		if(TextUtils.isEmpty(password)){//如果密码为空
+			return false;	
+		}else{
+			return true;
+		}
+		
+	}
+
+	/**
+	 * 
+	 * @author asus
+	 * 
+	 */
 	private class HomeAdapter extends BaseAdapter {
 
 		@Override
@@ -78,20 +166,24 @@ public class HomeActivity extends Activity {
 
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
-			//用打气筒将item_home显示到主界面
-			View view=View.inflate(HomeActivity.this, R.layout.item_home, null);
-			//查找view里面的控件,记住用view.find,否则空指针
-			ImageView icon=(ImageView) view.findViewById(R.id.iv_homeitem_icon);
+			// 用打气筒将item_home显示到主界面
+			View view = View.inflate(HomeActivity.this, R.layout.item_home,
+					null);
+			// 查找view里面的控件,记住用view.find,否则空指针
+			ImageView icon = (ImageView) view
+					.findViewById(R.id.iv_homeitem_icon);
 			icon.setImageResource(icons[position]);
-			TextView title=(TextView) view.findViewById(R.id.tv_homeitem_title);
+			TextView title = (TextView) view
+					.findViewById(R.id.tv_homeitem_title);
 			title.setText(names[position]);
-			TextView desc=(TextView) view.findViewById(R.id.tv_homeitem_desc);
+			TextView desc = (TextView) view.findViewById(R.id.tv_homeitem_desc);
 			desc.setText(descs[position]);
 			return view;
 		}
 	}
-	public void enterSettingActivity(View view){
-		Intent intent = new Intent(HomeActivity.this,SettingActivity.class);
+
+	public void enterSettingActivity(View view) {
+		Intent intent = new Intent(HomeActivity.this, SettingActivity.class);
 		startActivity(intent);
 	}
 
