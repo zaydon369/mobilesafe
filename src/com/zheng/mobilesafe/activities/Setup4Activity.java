fb7be5a4d9f25695deed7e548c3bc6286a444c2b
zaydon369 <zaydon369@yahoo.com>
2015-10-22 21:53:14 +0800

    手机防盗主界面UI基本完成,自动判断是否走过向导

--- src/com/zheng/mobilesafe/activities/Setup4Activity.java
+++ src/com/zheng/mobilesafe/activities/Setup4Activity.java
@@ -4,6 +4,8 @@ import com.zheng.mobilesafe.R;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -13,11 +15,15 @@ import android.widget.Button;
  * 设置向导,界面1
  */
 public class Setup4Activity extends Activity {
+	SharedPreferences sp;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_setup4);
+		//
+		sp=getSharedPreferences("config", MODE_PRIVATE);
+		//上一个的按钮
 		Button bt_pre = (Button) findViewById(R.id.bt_pre);
 		bt_pre.setOnClickListener(new OnClickListener() {
 			@Override
@@ -28,6 +34,21 @@ public class Setup4Activity extends Activity {
 				startActivity(intent);
 			}
 		});
-
+		Button bt_next = (Button) findViewById(R.id.bt_next);
+		bt_next.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(Setup4Activity.this,
+						LostFindActivity.class);
+				startActivity(intent);
+				//做个标记,告诉系统已经走过设置向导
+				Editor edit = sp.edit();
+				edit.putBoolean("configed", true);
+				edit.commit();
+				
+			}
+		});
 	}
 }
+
