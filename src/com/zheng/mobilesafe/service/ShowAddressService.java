bb9579c5a7bc5ba30d4df4fe718df61b112bfb65
zaydon369 <zaydon369@yahoo.com>
2015-10-29 14:23:37 +0800

    去电显示,吐司

--- src/com/zheng/mobilesafe/service/ShowAddressService.java
+++ src/com/zheng/mobilesafe/service/ShowAddressService.java
@@ -1,17 +1,24 @@
 package com.zheng.mobilesafe.service;
 
-import com.zheng.mobilesafe.db.dao.AddressDBDao;
-
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.Tag;
 import android.os.IBinder;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.widget.Toast;
 
-public class ShowAddressService extends Service {
+import com.zheng.mobilesafe.db.dao.AddressDBDao;
 
+public class ShowAddressService extends Service {
+	private static final String Tag = "MyPhoneListener";
+	private OutCallReceiver receiver;
+	TelephonyManager tm;
+	MyPhoneListener listener;
 	@Override
 	public IBinder onBind(Intent arg0) {
 		return null;
@@ -19,22 +26,29 @@ public class ShowAddressService extends Service {
 
 	@Override
 	public void onCreate() {
-		TelephonyManager tm = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
-		MyPhoneListener listener = new MyPhoneListener();
+		//去电状态监听.利用广播
+		receiver = new OutCallReceiver();
+		IntentFilter filter = new IntentFilter();
+		filter.addAction(Intent.ACTION_NEW_OUTGOING_CALL);
+		registerReceiver(receiver, filter);
+		//来电状态监听
+		tm = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+		listener = new MyPhoneListener();
 		tm.listen(listener, PhoneStateListener.LISTEN_CALL_STATE);
 		super.onCreate();
 	}
 
 	@Override
 	public void onDestroy() {
-		// TODO Auto-generated method stub
+		//取消去电监听
+		unregisterReceiver(receiver);
+		receiver=null;
+		//取消来电监听
+		tm.listen(listener, PhoneStateListener.LISTEN_NONE);
 		super.onDestroy();
 	}
 
 	private class MyPhoneListener extends PhoneStateListener {
-
-		private static final String Tag = "MyPhoneListener";
-
 		@Override
 		public void onCallStateChanged(int state, String incomingNumber) {
 			switch (state) {
@@ -56,4 +70,21 @@ public class ShowAddressService extends Service {
 		}
 
 	}
+	/**
+	 * 去电广播监听
+	 * @author asus
+	 *
+	 */
+	private class OutCallReceiver extends BroadcastReceiver{
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			//获取拨打出去的号码
+			String address=	AddressDBDao.findLocation(context, getResultData());
+			Log.i(Tag, "去电号码:"+address);
+			Toast.makeText(context, address, 0).show();
+		}
+		
+	}
+	
 }
