7309dc271830cf057842bc66250c1e960d330522
zaydon369 <zaydon369@yahoo.com>
2015-10-30 13:13:06 +0800

    手机剩余空间(小米不能正确识别SD卡)

--- src/com/zheng/mobilesafe/activities/AppManagerActivity.java
+++ src/com/zheng/mobilesafe/activities/AppManagerActivity.java
@@ -1,14 +1,31 @@
 package com.zheng.mobilesafe.activities;
 
-import com.zheng.mobilesafe.R;
+import java.text.Format;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.format.Formatter;
+import android.widget.TextView;
+
+import com.zheng.mobilesafe.R;
+import com.zheng.mobilesafe.activities.utils.SystemInfoUtils;
 
 public class AppManagerActivity extends Activity {
+	TextView tv_appmanager_internal;
+	TextView tv_appmanager_sdcard;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_app_manager);
+
+		tv_appmanager_internal = (TextView) findViewById(R.id.tv_appmanager_internal);
+		tv_appmanager_sdcard = (TextView) findViewById(R.id.tv_appmanager_sdcard);
+		//获取系统内存,给tv设置值
+		Long internal=SystemInfoUtils.getInternalStorageFreeSize();
+		Long sd=SystemInfoUtils.getSDStorageFreeSize();
+		//将文件大小格式化成易懂数据
+		tv_appmanager_internal.setText("机身存储剩余:"+Formatter.formatFileSize(getApplicationContext(), internal));
+		tv_appmanager_sdcard.setText("SD卡剩余:"+Formatter.formatFileSize(getApplicationContext(), sd));
 	}
 }
---
+++ src/com/zheng/mobilesafe/activities/utils/SystemInfoUtils.java
@@ -0,0 +1,55 @@
+package com.zheng.mobilesafe.activities.utils;
+
+import java.io.File;
+
+import android.content.Context;
+import android.os.Environment;
+
+/**
+ * 系统信息工具
+ */
+public class SystemInfoUtils {
+	/**
+	 * 获取内部存储空间的总大小
+	 * 
+	 * @return
+	 */
+	public static long getInternalStorageSize() {
+		// 得到系统目录
+		File file = Environment.getDataDirectory();
+		// 得到系统目录的总大小
+		return file.getTotalSpace();
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public static long getInternalStorageFreeSize() {
+		// 得到系统目录
+		File file = Environment.getDataDirectory();
+		// 得到系统目录的总大小
+		return file.getFreeSpace();
+	}
+
+	/**
+	 * 获取SD卡的总大小
+	 * 
+	 * @return
+	 */
+	public static long getSDStorageSize() {
+		File file = Environment.getExternalStorageDirectory();
+		return file.getTotalSpace();
+	}
+
+	/**
+	 * 获取SD卡剩余空间
+	 * 
+	 * @return
+	 */
+	public static long getSDStorageFreeSize() {
+		File file = Environment.getExternalStorageDirectory();
+		return file.getFreeSpace();
+
+	}
+}
