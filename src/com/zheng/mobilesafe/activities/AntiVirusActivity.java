7130bb650aae7dfedf444ac7b9d39edfa2a08198
zaydon369 <zaydon369@yahoo.com>
2015-11-04 16:49:49 +0800

    完成扫描过程和完成的UI显示,修复之前回调的bug,回调不能使用子线程

--- src/com/zheng/mobilesafe/activities/AntiVirusActivity.java
+++ src/com/zheng/mobilesafe/activities/AntiVirusActivity.java
@@ -1,7 +1,11 @@
 package com.zheng.mobilesafe.activities;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
@@ -14,68 +18,121 @@ import com.zheng.mobilesafe.activities.utils.ScanVirusUtil;
 import com.zheng.mobilesafe.activities.utils.ScanVirusUtil.CallBackup;
 
 public class AntiVirusActivity extends Activity {
+	protected static final int VIRUS_FILE = 0;
+	protected static final int SAFE_FILE = 1;
+	protected static final int SCAN_FINISH = 2;
+	private TextView tv_anti_scan_status;
+	private ProgressBar pb_anti_scan_status;
+	private LinearLayout ll_anti_container;
+	private ImageView iv_scan;
+
+	// 利用消息管理
+	Handler handler = new Handler() {
+		public void handleMessage(Message msg) {
+
+			switch (msg.what) {
+			case VIRUS_FILE://发现病毒文件
+				TextView tv_virus = new TextView(getApplicationContext());
+				tv_virus.setTextColor(Color.RED);
+				tv_virus.setText("发现病毒:" + msg.obj.toString());
+				ll_anti_container.addView(tv_virus, 0);
+				break;
+
+			case SAFE_FILE://安全文件
+				TextView tv_safe = new TextView(getApplicationContext());
+				tv_safe.setTextColor(Color.BLACK);
+				tv_safe.setText("扫描安全:" + msg.obj.toString());
+				ll_anti_container.addView(tv_safe, 0);
+				break;
+			case SCAN_FINISH://扫描完成
+				//将旋转动画图片移除
+				iv_scan.clearAnimation();
+				iv_scan.setVisibility(View.GONE);
+				tv_anti_scan_status.setText("扫描完成");
+				pb_anti_scan_status.setVisibility(View.GONE);
+				System.out.println("执行完上面方法后,关闭提示框..");
+				break;
+			}
+		};
+	};
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_anti_virus);
-		//找到需要用到的控件
-		ImageView iv_scan = (ImageView) findViewById(R.id.iv_anti_scan);
-		TextView tv_anti_scan_status=(TextView) findViewById(R.id.tv_anti_scan_status);
-		ProgressBar pb_anti_scan_status= (ProgressBar) findViewById(R.id.pb_anti_scan_status);
-		LinearLayout ll_anti_container=(LinearLayout) findViewById(R.id.ll_anti_container);
-		
-		//给扫描图片设置旋转
-		RotateAnimation ra=new RotateAnimation(0, 3600+1, Animation.RELATIVE_TO_SELF,0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+		iv_scan = (ImageView) findViewById(R.id.iv_anti_scan);
+		tv_anti_scan_status = (TextView) findViewById(R.id.tv_anti_scan_status);
+		pb_anti_scan_status = (ProgressBar) findViewById(R.id.pb_anti_scan_status);
+		ll_anti_container = (LinearLayout) findViewById(R.id.ll_anti_container);
+
+		// 给扫描图片设置旋转
+		RotateAnimation ra = new RotateAnimation(0, 3600 + 1,
+				Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
+				0.5f);
 		ra.setDuration(20000);
 		ra.setRepeatCount(Animation.INFINITE);
 		ra.setRepeatMode(Animation.RESTART);
 		iv_scan.startAnimation(ra);
-		
-		//开始查杀病毒
-		new Thread (){
-			public void run() {
-				ScanVirus();
-				try {
-					sleep(300);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			};
-		}.start();
-		
+
+		// 开始查杀病毒
+		ScanVirus();
 	}
-/**
- * 开始查杀病毒
- */
+
+	/**
+	 * 开始查杀病毒
+	 */
 	private void ScanVirus() {
-		ScanVirusUtil.scanVirus(getApplicationContext(), new CallBackup (){
-
+		new Thread() {
 			@Override
-			public void fileCounts(int count) {
-				
-				System.out.println("文件总数:"+count);
-				
+			public void run() {
+				ScanVirusUtil.scanVirus(getApplicationContext(),
+						new CallBackup() {
+							@Override
+							public void fileCounts(int count) {
+								int close = count;
+								System.out.println("文件总数:" + count);
+								pb_anti_scan_status.setMax(count);
+							}
+
+							@Override
+							public void fileProgress(int pro) {
+								pb_anti_scan_status.setProgress(pro);
+								System.out.println("当前进度:" + pro);
+								try {
+									//扫描一个,休息0.1秒
+									sleep(100);
+								} catch (InterruptedException e) {
+									// TODO Auto-generated catch block
+									e.printStackTrace();
+								}
+							}
+
+							@Override
+							public void virusFile(String virusFile) {
+								System.out.println("病毒文件:" + virusFile);
+								Message msg = Message.obtain();
+								msg.what = VIRUS_FILE;
+								msg.obj = virusFile;
+								handler.sendMessage(msg);
+
+							}
+
+							@Override
+							public void safetyFile(String safetyFile) {
+								System.out.println("安全文件:" + safetyFile);
+								Message msg = Message.obtain();
+								msg.what = SAFE_FILE;
+								msg.obj = safetyFile;
+								handler.sendMessage(msg);
+							}
+						});
+				// 执行完上面方法后,关闭提示框
+				Message msg = Message.obtain();
+				msg.what = SCAN_FINISH;
+				handler.sendMessage(msg);
+				super.run();
 			}
+		}.start();
 
-			@Override
-			public void fileProgress(int pro) {
-				System.out.println("当前进度:"+pro);
-				
-			}
-
-			@Override
-			public void virusFile(String virusFile) {
-				System.out.println("病毒文件:"+virusFile);
-				
-			}
-
-			@Override
-			public void safetyFile(String safetyFile) {
-				System.out.println("安全文件"+safetyFile);
-				
-			}});
-		
 	}
-
 }
--- src/com/zheng/mobilesafe/activities/utils/ScanVirusUtil.java
+++ src/com/zheng/mobilesafe/activities/utils/ScanVirusUtil.java
@@ -30,34 +30,31 @@ public class ScanVirusUtil {
 	 */
 	public static void scanVirus(final Context context, final CallBackup backup) {
 
-		new Thread() {
-			public void run() {
-				// 得到所有app的安装路径
-				PackageManager pm = context.getPackageManager();
-				List<PackageInfo> infos = pm.getInstalledPackages(0);
-				// 进度的最大值,也就是有多少个APP文件目录
-				backup.fileCounts(infos.size());
-				// 扫描的进度值
-				int pro = 1;
-				// 遍历所有App路径
-				for (PackageInfo info : infos) {
-					String path = info.applicationInfo.sourceDir;
-					String md5 = Md5Utils.getFileMd5(path);
-					// 查询数据库是不是有这个记录
-					String virus = ScanVirusDao.ScanVirus(context, md5);
-					// 判断文件是否是病毒
-					if (TextUtils.isEmpty(virus)) {
-						// 如果为空说明没病毒
-						backup.safetyFile(info.applicationInfo.loadLabel(pm)+"");
-					} else {
-						// 否则说明是病毒
-						backup.virusFile(info.applicationInfo.loadLabel(pm)+"");
-					}
-					// 进度自身+1
-					backup.fileProgress(pro++);
-				}
-			};
-		}.start();
+		// 得到所有app的安装路径
+		PackageManager pm = context.getPackageManager();
+		List<PackageInfo> infos = pm.getInstalledPackages(0);
+		// 进度的最大值,也就是有多少个APP文件目录
+		backup.fileCounts(infos.size());
+		// 扫描的进度值
+		int pro = 1;
+		// 遍历所有App路径
+		for (PackageInfo info : infos) {
+			String path = info.applicationInfo.sourceDir;
+			String md5 = Md5Utils.getFileMd5(path);
+			// 查询数据库是不是有这个记录
+			String virus = ScanVirusDao.ScanVirus(context, md5);
+			// 判断文件是否是病毒
+			if (TextUtils.isEmpty(virus)) {
+				// 如果为空说明没病毒
+				backup.safetyFile(info.applicationInfo.loadLabel(pm) + "");
+			} else {
+				// 否则说明是病毒
+				backup.virusFile(info.applicationInfo.loadLabel(pm) + "");
+			}
+			// 进度自身+1
+			backup.fileProgress(pro++);
+
+		}
 
 	}
 
