7502283d0194c5c2cf5f913ec692eddfd2b8fffc
zaydon369 <zaydon369@yahoo.com>
2015-11-05 13:05:22 +0800

    修正系统加速的一些UI布局错误,Fragment的容器使用帧布局,修正之前的图标放错位置

--- src/com/zheng/mobilesafe/activities/SystemOptisActivity.java
+++ src/com/zheng/mobilesafe/activities/SystemOptisActivity.java
@@ -6,6 +6,7 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -16,7 +17,7 @@ import com.zheng.mobilesafe.fragment.CleanSDFragment;
 
 public class SystemOptisActivity extends FragmentActivity implements
 		OnClickListener {
-	private LinearLayout ll_system_optis_fragment;
+	private FrameLayout ll_system_optis_fragment;
 	private LinearLayout ll_system_optis_cache;
 	private LinearLayout ll_system_optis_sdcar;
 	private TextView tv_system_optis_title;
@@ -32,7 +33,7 @@ public class SystemOptisActivity extends FragmentActivity implements
 		ll_system_optis_cache = (LinearLayout) findViewById(R.id.ll_system_optis_cache);
 		ll_system_optis_sdcar = (LinearLayout) findViewById(R.id.ll_system_optis_sdcar);
 		tv_system_optis_title = (TextView) findViewById(R.id.tv_system_optis_title);
-		ll_system_optis_fragment = (LinearLayout) findViewById(R.id.ll_system_optis_fragment);
+		ll_system_optis_fragment = (FrameLayout) findViewById(R.id.ll_system_optis_fragment);
 		iv_system_optis_cache = (ImageView) findViewById(R.id.iv_system_optis_cache);
 		tv_system_optis_cache = (TextView) findViewById(R.id.tv_system_optis_cache);
 		iv_system_optis_sdcar = (ImageView) findViewById(R.id.iv_system_optis_sdcar);
@@ -65,19 +66,22 @@ public class SystemOptisActivity extends FragmentActivity implements
 	 * 跳到清除sd卡的Fragment
 	 */
 	private void getCleanSdCarFragment() {
-		//更改标题
+		// 更改标题
 		tv_system_optis_title.setText("SD卡清理");
-		//更改另一个按钮的背景图片和文字颜色
-				ll_system_optis_cache.setBackgroundResource(0);
-				 iv_system_optis_cache.setBackgroundResource(R.drawable.clean_sdcard_icon);
-				 tv_system_optis_cache.setTextColor(0xbb000000);
-				 ll_system_optis_sdcar.setBackgroundResource(R.drawable.dg_cancel_selector);
-				iv_system_optis_sdcar.setBackgroundResource(R.drawable.clean_cache_icon_pressed);
-				tv_system_optis_sdcar.setTextColor(0xFF7bB226);
-	
+		// 更改另一个按钮的背景图片和文字颜色
+		ll_system_optis_cache.setBackgroundResource(0);
+		iv_system_optis_cache
+				.setBackgroundResource(R.drawable.clean_cache_icon);
+		tv_system_optis_cache.setTextColor(0xbb000000);
+		ll_system_optis_sdcar
+				.setBackgroundResource(R.drawable.dg_cancel_selector);
+		iv_system_optis_sdcar
+				.setBackgroundResource(R.drawable.clean_sdcard_icon_pressed);
+		tv_system_optis_sdcar.setTextColor(0xFF7bB226);
 
-		//填充Fragment
-		FragmentTransaction ft_sdcar = getSupportFragmentManager().beginTransaction();
+		// 填充Fragment
+		FragmentTransaction ft_sdcar = getSupportFragmentManager()
+				.beginTransaction();
 		ft_sdcar.replace(R.id.ll_system_optis_fragment, new CleanSDFragment());
 		ft_sdcar.commit();
 	}
@@ -87,13 +91,16 @@ public class SystemOptisActivity extends FragmentActivity implements
 	 */
 	private void getCleanCacheFragment() {
 		tv_system_optis_title.setText("缓存清理");
-		
-		//更改另一个按钮的背景图片和文字颜色
-		ll_system_optis_cache.setBackgroundResource(R.drawable.dg_cancel_selector);
-		 iv_system_optis_cache.setBackgroundResource(R.drawable.clean_sdcard_icon_pressed);
-		 tv_system_optis_cache.setTextColor(0xFF7bB226);
-		 ll_system_optis_sdcar.setBackgroundResource(0);
-		iv_system_optis_sdcar.setBackgroundResource(R.drawable.clean_cache_icon);
+
+		// 更改另一个按钮的背景图片和文字颜色
+		ll_system_optis_cache
+				.setBackgroundResource(R.drawable.dg_cancel_selector);
+		iv_system_optis_cache
+				.setBackgroundResource(R.drawable.clean_cache_icon_pressed);
+		tv_system_optis_cache.setTextColor(0xFF7bB226);
+		ll_system_optis_sdcar.setBackgroundResource(0);
+		iv_system_optis_sdcar
+				.setBackgroundResource(R.drawable.clean_sdcard_icon);
 		tv_system_optis_sdcar.setTextColor(0xbb000000);
 
 		FragmentTransaction ft_cache = getSupportFragmentManager()
@@ -104,4 +111,3 @@ public class SystemOptisActivity extends FragmentActivity implements
 	}
 
 }
-
