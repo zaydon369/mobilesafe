978f0ac779f8189a5b22d080fabc33efd49f0e43
zaydon369 <zaydon369@yahoo.com>
2015-11-07 20:37:17 +0800

    完成通过广播发送白名单,暂时取消拦截

--- src/com/zheng/mobilesafe/activities/EnterPasswordActivity.java
+++ src/com/zheng/mobilesafe/activities/EnterPasswordActivity.java
@@ -15,55 +15,64 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.zheng.mobilesafe.R;
+
 /**
  * 程序锁,输入密码提示框
+ * 
  * @author asus
- *
+ * 
  */
 public class EnterPasswordActivity extends Activity {
-private ImageView iv_enter_password_icon;
-private TextView tv_enter_password_appname;
-private EditText et_enter_password_pwd;
-private Button bt_enter_password_confirm;
+	private ImageView iv_enter_password_icon;
+	private TextView tv_enter_password_appname;
+	private EditText et_enter_password_pwd;
+	private Button bt_enter_password_confirm;
+	private String packageName;
 
-@Override
-protected void onCreate(Bundle savedInstanceState) {
-	super.onCreate(savedInstanceState);
-	setContentView(R.layout.activity_enter_password);
-	iv_enter_password_icon = (ImageView) findViewById(R.id.iv_enter_password_icon);
-	tv_enter_password_appname = (TextView) findViewById(R.id.tv_enter_password_appname);
-	et_enter_password_pwd = (EditText) findViewById(R.id.et_enter_password_pwd);
-	bt_enter_password_confirm = (Button) findViewById(R.id.bt_enter_password_confirm);
-	
-	
-	
-	
-	Intent intent = getIntent();
-	String packageName=intent.getStringExtra("packageName");
-	System.out.println("意图得到的包名:"+packageName);
-	PackageManager pm=getPackageManager();
-	try {
-		ApplicationInfo applicationInfo = pm.getApplicationInfo(packageName, 0);
-		iv_enter_password_icon.setBackgroundDrawable(applicationInfo.loadIcon(pm));
-		tv_enter_password_appname.setText(applicationInfo.loadLabel(pm));
-	} catch (NameNotFoundException e) {
-		e.printStackTrace();
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_enter_password);
+		iv_enter_password_icon = (ImageView) findViewById(R.id.iv_enter_password_icon);
+		tv_enter_password_appname = (TextView) findViewById(R.id.tv_enter_password_appname);
+		et_enter_password_pwd = (EditText) findViewById(R.id.et_enter_password_pwd);
+		bt_enter_password_confirm = (Button) findViewById(R.id.bt_enter_password_confirm);
+
+		Intent intent = getIntent();
+		packageName = intent.getStringExtra("packageName");
+		System.out.println("意图得到的包名:" + packageName);
+		PackageManager pm = getPackageManager();
+		try {
+			ApplicationInfo applicationInfo = pm.getApplicationInfo(
+					packageName, 0);
+			iv_enter_password_icon.setBackgroundDrawable(applicationInfo
+					.loadIcon(pm));
+			tv_enter_password_appname.setText(applicationInfo.loadLabel(pm));
+		} catch (NameNotFoundException e) {
+			e.printStackTrace();
+		}
+		bt_enter_password_confirm.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String pwd = et_enter_password_pwd.getText().toString().trim();
+				if (pwd.isEmpty()) {
+					Toast.makeText(getApplicationContext(), "请输入密码", 0).show();
+					return;
+				}
+				if (pwd.equals("123")) {
+					// 密码正确,发送广播,通知看门狗,不要拦截
+					Intent intent = new Intent();
+					intent.setAction("com.zheng.mobilesafe.tempstopprotect");
+					intent.putExtra("packageName", packageName);
+					sendBroadcast(intent);
+
+					finish();
+				} else {
+					Toast.makeText(getApplicationContext(), "密码错误", 0).show();
+					return;
+				}
+
+			}
+		});
 	}
-	bt_enter_password_confirm.setOnClickListener(new OnClickListener(){
-		@Override
-		public void onClick(View v) {
-			String pwd=et_enter_password_pwd.getText().toString().trim();
-			if(pwd.isEmpty()){
-				Toast.makeText(getApplicationContext(), "请输入密码", 0).show();
-				return;
-			}
-			if(pwd.equals("123")){
-				finish();
-			}else{
-				Toast.makeText(getApplicationContext(), "密码错误", 0).show();
-				return;	
-			}
-			
-		}});
-}
 }
--- src/com/zheng/mobilesafe/service/WatchDogService.java
+++ src/com/zheng/mobilesafe/service/WatchDogService.java
@@ -9,7 +9,10 @@ import com.zheng.mobilesafe.db.dao.AppLockDao;
 import android.app.ActivityManager;
 import android.app.ActivityManager.RunningTaskInfo;
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.IBinder;
 import android.os.SystemClock;
 
@@ -21,7 +24,9 @@ public class WatchDogService extends Service {
 	private boolean flag;
 	// 加锁应用程序列表
 	private ArrayList<String> allLockapps;
-
+	private IntentReceiver receiver;
+	//临时取消保护的列表
+	ArrayList<String> whiteApps;
 	@Override
 	public IBinder onBind(Intent intent) {
 		return null;
@@ -34,7 +39,14 @@ public class WatchDogService extends Service {
 		// 当没有加锁程序时就为false,停止循环,减少cup负担
 		flag = !(allLockapps.isEmpty());
 		am = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
-
+		//初始化广播接受者
+		receiver = new IntentReceiver();
+		IntentFilter filter=new IntentFilter();
+		filter.addAction("com.zheng.mobilesafe.tempstopprotect");
+		//注册广播接受者
+		registerReceiver(receiver, filter);
+		//初始化白名单列表
+		whiteApps=new ArrayList<String>();
 		new Thread() {
 			//将循环写在子线程里,避免anr,应用程序无响应
 			public void run() {
@@ -43,6 +55,8 @@ public class WatchDogService extends Service {
 				String packageName;
 				// 循环查找当前的栈
 				while (flag) {
+					//睡眠,避免监控频繁造成浪费
+					SystemClock.sleep(30);
 					// 获取正在运行的activity的栈
 					runningTasks = am.getRunningTasks(1);
 					// 得到当前正在运行的包名
@@ -50,15 +64,18 @@ public class WatchDogService extends Service {
 							.getPackageName();
 					// 当当前运行的程序在加锁列表时,弹出提示
 					if (allLockapps.contains(packageName)) {
-						//System.out.println(packageName + "在加锁列表");
+						//判断是否在白名单里面
+						if(whiteApps!=null && whiteApps.contains(packageName)){
+							//如果在白名单里面直接跳过本次循环
+							continue;
+						};
 						//弹出对话框,提示输入密码
 						Intent intent=new Intent(getApplicationContext(), EnterPasswordActivity.class);
 						intent.putExtra("packageName", packageName);
 						intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 						startActivity(intent);
 					}
-					//睡眠,避免监控频繁造成浪费
-					SystemClock.sleep(30);
+					
 				}
 			};
 		}.start();
@@ -69,7 +86,23 @@ public class WatchDogService extends Service {
 	@Override
 	public void onDestroy() {
 		flag = false;
+		if(receiver!=null){
+		unregisterReceiver(receiver);
+		}
 		super.onDestroy();
 	}
+	/**
+	 * 接收密码框传过来的取消暂时保护的包名
+	 * @author asus
+	 *
+	 */
+class IntentReceiver extends BroadcastReceiver{
 
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		if(whiteApps.size()>1){//暂且将白名单列表设置为1
+			whiteApps.remove(0);
+		}
+		whiteApps.add(intent.getStringExtra("packageName"));
+	}}
 }
